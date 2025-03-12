fd0dcf94b916c53127960fd3ee1084a778498ebd
nvhoa2005 <hoa049170@gmail.com>
2025-03-12 23:55:02 +0700

    update hero

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ navbarLinks.forEach(navbarLink => {
         navbarLink.classList.remove("navbar__link--active")
     })
 });
+
+// ==================== Hero ========================
+subDots = document.querySelectorAll(".hero__content--subDot svg")
+subDots.forEach(subDot => {
+    subDot.addEventListener("mouseover", () => {
+        subDot.classList.add("hero__content--subDot-active")
+    })
+    subDot.addEventListener("mouseout", () => {
+        subDot.classList.remove("hero__content--subDot-active")
+    })
+});
